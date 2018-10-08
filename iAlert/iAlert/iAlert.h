3314003b5bf89f5ababc6e3ca3d797bb10215852
Irshad PC <ichu501@gmail.com>
2018-10-08 22:49:12 +0530

    Initial setup

---
+++ iAlert/iAlert/iAlert.h
@@ -0,0 +1,19 @@
+//
+//  iAlert.h
+//  iAlert
+//
+//  Created by Irshad PC on 08/10/18.
+//  Copyright © 2018 Irshad PC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iAlert.
+FOUNDATION_EXPORT double iAlertVersionNumber;
+
+//! Project version string for iAlert.
+FOUNDATION_EXPORT const unsigned char iAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iAlert/PublicHeader.h>
+
+
